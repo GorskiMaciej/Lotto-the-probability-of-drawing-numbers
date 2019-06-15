c8d32abd95592577b797fd42e6d821bd0ed4c5da
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-06-15 22:48:39 +0200

    initial script

---
+++ SCRIPTS/script.js
@@ -0,0 +1,21 @@
+class Lotto {
+
+}
+class ColorChanger {
+    static colorChanger() {
+
+    }
+}
+
+// class Probability {
+//     constructor() {
+//         document.querySelector('button.lotto').addEventListener('click', this.startLotto);
+//         document.querySelector('button.lotto').addEventListener('click', () => {
+
+//         }));
+// }
+startLotto() {
+    this.lottoProbability = new Lotto();
+}
+}
+const Probability = new Probability();
