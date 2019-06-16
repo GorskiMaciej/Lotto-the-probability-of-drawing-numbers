ef913174c9f216e586e7080e37f4073338656082
MACIEJ-KOMPUTER\Maciej <gorski.maciej2@gmail.com>
2019-06-16 22:14:34 +0200

    Trying to change color of frame and turn on param

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,21 +1,54 @@
-class Lotto {
-
-}
 class ColorChanger {
-    static colorChanger() {
-
+    constructor() {
+        let _color = this.changeColor;
+        this.getColor = () => _color;
+        this.changeColor();
+    }
+    changeColor(colorFromBtn, parametersFrame) {
+        this.parametersFrame.style.border = `3px solid ${getComputedStyle(e.target).backgroundColor}`;
     }
 }
 
-// class Probability {
-//     constructor() {
-//         document.querySelector('button.lotto').addEventListener('click', this.startLotto);
-//         document.querySelector('button.lotto').addEventListener('click', () => {
+class Probability {
+    constructor() {
 
-//         }));
-// }
-startLotto() {
-    this.lottoProbability = new Lotto();
+
+        this.btnLotto = document.querySelector('button.lotto');
+        this.btnMiniLotto = document.querySelector('button.miniLotto');
+        this.btnMultiMulti = document.querySelector('button.multiMulti');
+        this.btnKeno = document.querySelector('button.keno');
+        this.parametersFrame = document.querySelector('.parameters');
+        this.btnCalculate = document.querySelector('button.calculate');
+        this.numberOfBetsParameter = document.querySelector('.numberOfBets');
+        this.btnLotto.addEventListener('click', this.startLotto)
+        this.btnMiniLotto.addEventListener('click', (e) => {
+            // console.log(e.target);
+            // console.log(this.parametersFrame);
+            // console.log(getComputedStyle(e.target).backgroundColor);
+            this.parametersFrame.style.border = `3px solid ${getComputedStyle(e.target).backgroundColor}`;
+        })
+        this.btnMultiMulti.addEventListener('click', (e) => {})
+        this.btnKeno.addEventListener('click', (e) => {})
+
+        console.log("1")
+        console.log(this.numberOfBetsParameter)
+    }
+
+    startLotto() {
+        this.lottoProbability = new Lotto(this.numberOfBetsParameter);
+        // this.colorChanger = new this.colorChanger();
+    }
+
 }
+class Lotto {
+    constructor(numberOfBetsParameter) {
+        console.log("inicjalizacja obiektu powiodła się")
+        console.log(numberOfBetsParameter)
+        this.turnOnParameters(numberOfBetsParameter);
+    }
+    turnOnParameters(numberOfBetsParameter) {
+        // numberOfBetsParameter.classList.addClass("active");
+    }
 }
-const Probability = new Probability();
+
+const probability = new Probability();
